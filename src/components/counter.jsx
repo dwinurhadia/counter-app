677af33dfca14d684b32d4261013302951b757eb
Dwi Nurhadi Afriyanto <dwi.nurhadi17@student.uns.ac.id>
2020-07-09 10:29:49 +0700

    add hello world in react component

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,10 +1,8 @@
 import React, { Component } from 'react';
-
-class  extends Component {
-    state = {  }
-    render() { 
-        return (  );
+class Counter extends Component {
+    render() {         
+        return <h1>Hello World</h1>;
     }
 }
  
-export default ;
+export default Counter;
