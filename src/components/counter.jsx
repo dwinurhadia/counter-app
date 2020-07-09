5c090f79f4a21f37f734ef3b90c611d8df0d2fb5
Dwi Nurhadi Afriyanto <dwi.nurhadi17@student.uns.ac.id>
2020-07-09 14:57:53 +0700

    add tag state

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
 class Counter extends Component {
     state = {
-        count: 0
+        count: 0,
+        tags: ['tag1','tag2','tag3']
     };
     
     style = {
@@ -10,12 +11,13 @@ class Counter extends Component {
     };
 
     render() {
-        let classes = this.getBadgeClasses();
-
         return (
             <div>
-                <span style = {this.style} className={classes}>{this.formatCount()}</span>
-                <button className="btn btn-secondary btn-sm">Increment</button> 
+                <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
+                <button className="btn btn-secondary btn-sm">Increment</button>
+                <ul>
+                    { this.state.tags.map(tag => <li key={tag}>{tag}</li>)}
+                </ul> 
             </div>
         );
     }
