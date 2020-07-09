57f465a2805ac59928b51170096b229f6218623f
Dwi Nurhadi Afriyanto <dwi.nurhadi17@student.uns.ac.id>
2020-07-09 11:43:55 +0700

    add style for component

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -3,13 +3,18 @@ class Counter extends Component {
     state = {
         count: 0
     };
+    
+    style = {
+        fontSize : 10,
+        fontWeight : "bold"
+    };
 
     render() {         
         return (
             <div>
-                <span className="badge badge-primary m-2">{this.formatCount()}</span>
-                <button>Increment</button> 
-                </div>
+                <span style = {this.style} className="badge badge-primary m-2">{this.formatCount()}</span>
+                <button className="btn btn-secondary btn-sm">Increment</button> 
+            </div>
         );
     }
     formatCount(){
